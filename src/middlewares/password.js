4bed26cf751cfaedb750b2d8530becbfecf148e0
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-08-07 14:14:17 -0300

    Requisito 4

--- src/middlewares/password.js
+++ src/middlewares/password.js
@@ -1,4 +1,13 @@
-// const password = (req, res, next) => {
-//     const { password } = req.body;
+const validationPassword = (req, res, next) => {
+    const { password } = req.body;
 
-// };
+    if (!password) {
+        return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    } 
+    if (password.length < 6) {
+        return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    }
+    next();
+};
+
+module.exports = validationPassword;
--- src/routers/login.router.js
+++ src/routers/login.router.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const crypto = require('crypto');
+const validationEmail = require('../middlewares/email');
+const validationPassword = require('../middlewares/password');
 
 const loginsRouter = express.Router();
 
 function createToken() {
     return crypto.randomBytes(8).toString('hex');
 }
+
+loginsRouter.use(validationEmail, validationPassword);
 
 loginsRouter.post('/', (req, res) => {
    const token = createToken();
